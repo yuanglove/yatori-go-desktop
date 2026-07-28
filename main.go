ccc38c887467bc9a7169abd1b4e8915fa4a0d1c1
yuan <3586285910@qq.com>
2026-07-28 13:04:02 +0800

    release: finalize v0.4.0 desktop controls

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 var assets embed.FS
 
 func main() {
-	cliMode   := flag.Bool("cli", false, "以 CLI 模式运行")
+	cliMode := flag.Bool("cli", false, "以 CLI 模式运行")
 	workerMode := flag.Bool("worker", false, "内部 worker 模式")
 	workerUID := flag.String("uid", "", "worker 模式：任务 UID")
 	flag.Parse()
@@ -30,18 +30,27 @@ func main() {
 		return
 	}
 
+	releaseInstance, alreadyRunning, err := acquireDesktopInstance()
+	if err != nil {
+		panic(err)
+	}
+	if alreadyRunning {
+		return
+	}
+	defer releaseInstance()
+
 	app := NewApp()
-	err := wails.Run(&options.App{
-		Title:  "Yatori 学习管理工具",
-		Width:  1200,
-		Height: 780,
-		MinWidth:  900,
-		MinHeight: 600,
-		AssetServer: &assetserver.Options{Assets: assets},
+	err = wails.Run(&options.App{
+		Title:            "Yatori 学习管理工具",
+		Width:            1200,
+		Height:           780,
+		MinWidth:         900,
+		MinHeight:        600,
+		AssetServer:      &assetserver.Options{Assets: assets},
 		BackgroundColour: &options.RGBA{R: 18, G: 18, B: 18, A: 1},
-		OnStartup:  app.startup,
-		OnShutdown: app.shutdown,
-		Bind:       []interface{}{app},
+		OnStartup:        app.startup,
+		OnShutdown:       app.shutdown,
+		Bind:             []interface{}{app},
 		Windows: &windows.Options{
 			WebviewIsTransparent: false,
 			WindowIsTranslucent:  false,
